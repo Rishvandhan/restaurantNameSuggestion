4d7a4566f6f292669cc9d6d2ceeb3c2115ed0e66
Rishvandhan R <rishvndhan.r@gmail.com>
2025-04-28 19:58:43 +0530

    3

--- helper.py
+++ helper.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 
 
-config = toml.load(".streamlit\secrets.toml")
+config = toml.load(".streamlit/secrets.toml")
 
 os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
 os.environ["OPENAI_API_KEY"] = config["secrets"]["OPENAI_API_KEY"]
